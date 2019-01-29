857f3084024838941c6d89fcabe6ec265bfea97c
Brindha2401 <43044145+Brindha2401@users.noreply.github.com>
2019-01-29 11:17:05 +0530

    Servlet 3.0

--- src/com/servlet/OnServletLogin.java
+++ src/com/servlet/OnServletLogin.java
@@ -1,14 +1,15 @@
 package com.servlet;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/login")
 public class OnServletLogin extends HttpServlet  
 {
     protected void doPost(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException
